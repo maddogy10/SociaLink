2719dea5f59cc7a623a5e21d82b1291bb11f42de
Maddy Young <maddogy101010@gmail.com>
2025-10-12 23:36:03 -0500

    Added footer icons and initial useState and useEffect functionalities to profilecard.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,19 +8,19 @@ const Footer = () => {
   </div>
   <div className="socials">
     <button className="facebook">
-      <img src="https://i.imgur.com/p9kFnh5.png" alt="facebook logo"/>
+      <img src='src/assets/Facebook.png' alt="facebook logo"/>
     </button>
     <button className="twitter">
-      <img src="https://i.imgur.com/cPGJPOX.png" alt="twitter logo"/>
+      <img src="src/assets/Twitter.png" alt="twitter logo"/>
     </button>
     <button className="instagram">
-      <img src="https://i.imgur.com/pBTGwX1.png" alt="instagram logo"/>
+      <img src="src/assets/Instagram.png" alt="instagram logo"/>
     </button>
     <button className="linkedin">
-      <img src="https://i.imgur.com/Dso2Bvm.png" alt="linkedin logo"/>
+      <img src="src/assets/LinkedIn.png" alt="linkedin logo"/>
     </button>
     <button className="youtube">
-      <img src="https://i.imgur.com/DnNiutu.png" alt="youtube logo"/>
+      <img src="src/assets/YouTube.png" alt="youtube logo"/>
     </button>
   </div>
 </div>
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
 
 const ProfileCard = ({name, age, major, image}) => {
    // const givenMajor = {major};
+   //#0077B6
+   const [color, setColor] = useState("#CAF0F8");
     const schoolMajor = major.toUpperCase();
+    useEffect(() => {
+      console.log("Color has changed");
+    })
     return (
-    <div className="profBlock">
+    <div className="profBlock" style={{ backgroundColor: color}} onMouseEnter={() => setColor("#0077B6")} onMouseLeave={() => setColor("#CAF0F8")}>
       <div className="profImg">
         <img src={image} alt="profile icon"/>
       </div>
